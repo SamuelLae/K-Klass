f8f98790cba6d467f6ef82ea4b479fa86d0e9ea2
samuel <samuel.laestander@edu.lycksele.se>
2025-01-31 13:16:13 +0100

    Slutprodukt

--- Kö/obj/Debug/net8.0/Kö.AssemblyInfo.cs
+++ Kö/obj/Debug/net8.0/Kö.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Kö")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de600633a6f0993b57ddc37c853252307d9a4d4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79ad3980283cfd788cec2215029e563c7ce1df84")]
 [assembly: System.Reflection.AssemblyProductAttribute("Kö")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Kö")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
